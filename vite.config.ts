31a596c869fad8ce7b941fa54e69f99af9665c0f
Spongzi <lxlandsx@outlook.com>
2021-12-26 21:34:11 +0800

    王者学院 前端:vue+vite+ts 初始化

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
+const resolve = (dir:string) => path.join(__dirname, dir)
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve:{
+    alias:{
+      "@":resolve("src"),
+      comps: resolve("src/components"),
+      apis: resolve("src/apis"),
+      views: resolve("src/views"),
+      utils: resolve("src/utils"),
+      routes: resolve("src/routes"),
+      styles: resolve("src/styles"),
+      plugins:resolve("src/plugins"),
+      page:resolve("src/page")
+    }
+  }
 })
